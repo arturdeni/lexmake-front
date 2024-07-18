46a742ec337ec2f8ed405e8745b5c8d24ad0374f
arturdeni <arturde@ext.zara.com>
2024-07-18 09:28:56 +0200

    crear scroll horizontal para HomeProjects

--- src/components/Header/MenuButton/MenuButton.js
+++ src/components/Header/MenuButton/MenuButton.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./MenuButton.css";
 
 const MenuButton = ({ toggleMenu, isOpen }) => {
-  console.log("isOpen: ", isOpen);
   return (
     <button className="menu-button" onClick={toggleMenu}>
       <div className={`menu-button__line ${isOpen ? "open" : ""}`}></div>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Home.css";
 
+import HomeProjects from "./HomeProjects/HomeProjects";
+
 const Home = () => {
   return (
     <div className="home">
@@ -13,30 +15,7 @@ const Home = () => {
         </p>
       </div>
 
-      <div className="home__projects">
-        <h2>Proyectos destacados</h2>
-        <div className="home__project">
-          <img
-            src="https://images.unsplash.com/photo-1501127122-f385ca6ddd9d?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="Proyecto 1"
-          />
-          <h3>Proyecto 1</h3>
-        </div>
-        <div className="home__project">
-          <img
-            src="https://plus.unsplash.com/premium_photo-1682125317942-013b0235b261?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="Proyecto 2"
-          />
-          <h3>Proyecto 2</h3>
-        </div>
-        <div className="home__project">
-          <img
-            src="https://images.unsplash.com/photo-1516131206008-dd041a9764fd?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="Proyecto 3"
-          />
-          <h3>Proyecto 3</h3>
-        </div>
-      </div>
+      <HomeProjects />
 
       <div className="home__services">
         <h2>Servicios</h2>
