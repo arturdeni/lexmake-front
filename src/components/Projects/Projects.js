745657fba072c363f003ee4896ab6ce87df141ba
arturdeni <arturde@ext.zara.com>
2024-09-12 18:58:44 +0200

    animaciones de proyectos tanto en home como en projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,18 +1,53 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Link } from "react-router-dom";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/all";
 
 import HaveAnIdea from "../shared/HaveAnIdea/HaveAnIdea";
 import projectsData from "../../services/projects.json";
-
 import "./Projects.css";
 
+gsap.registerPlugin(ScrollTrigger);
+
 const Projects = () => {
+  const projectsRef = useRef([]);
+
+  useGSAP(() => {
+    if (projectsRef.current.length > 0) {
+      projectsRef.current.forEach((item, index) => {
+        gsap.fromTo(
+          item,
+          {
+            opacity: 0,
+            y: 50,
+          },
+          {
+            opacity: 1,
+            y: 0,
+            duration: 1,
+            ease: "power3.out", // Efecto suave en la animación
+            scrollTrigger: {
+              trigger: item,
+              start: "top 80%",
+              toggleActions: "play none none reverse", // Reproduce la animación cuando el item entra en el viewport
+            },
+          }
+        );
+      });
+    }
+  }, []);
+
   return (
     <div className="projects">
       <h1 className="slide-in-blurred-top">Proyectos</h1>
       <ul>
-        {projectsData.projects.map((project) => (
-          <li key={project.id} className="projects-item">
+        {projectsData.projects.map((project, index) => (
+          <li
+            key={project.id}
+            className="projects-item"
+            ref={(el) => (projectsRef.current[index] = el)} // Guarda una referencia para cada item
+          >
             <Link to={`/projects/${project["url-title"]}`}>
               <img
                 src={project.frontImage}
